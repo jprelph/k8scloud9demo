efab7fdd66a59f7a919b16558f2a232968edde03
James Relph <james.relph@capgemini.com>
2020-01-15 13:50:45 +0000

    Add sha256 checks

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 cd /tmp
+curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/darwin/amd64/kubectl
 curl -o kubectl.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl.sha256
+sha256sum -c kubectl.sha256 || exit 1
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 echo 'Kubectl version is: $(kubectl version --short --client)
 echo 'Installing aws-iam-authenticator'
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
+curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator.sha256
+sha256sum -c aws-iam-authenticator.sha256 || exit 1
 chmod +x ./aws-iam-authenticator
 mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
 aws-iam-authenticator help
-
