4744c31214d08156a75f5b79a8bbabbc09bfc13a
James Relph <james.relph@capgemini.com>
2020-01-15 13:56:15 +0000

    Fix quotes

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -7,7 +7,7 @@ sha256sum -c kubectl.sha256 || exit 1
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-echo 'Kubectl version is: $(kubectl version --short --client)
+echo 'Kubectl version is: $(kubectl version --short --client)'
 echo 'Installing aws-iam-authenticator'
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
 curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator.sha256
